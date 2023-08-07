a5d6f84934d55eb43348752813f038bd92a7ae2d
Kelvin de Reus <31496522+kelvin-codes-stuff@users.noreply.github.com>
2023-08-07 22:45:23 +0200

    Update removed print

--- cogs/bump_reminder.py
+++ cogs/bump_reminder.py
@@ -34,14 +34,12 @@ class Bump_reminder(commands.Cog):
                     for embed in embeds:
                         try:
                             if "Check it out [on DISBOARD](https://disboard.org/server/716388937450389514)." in str(embed.description) and message.id not in self.bumped_messages:
-                                print("Jeej")
                                 datetime_object_message = datetime.strptime(str(message.created_at).split(".")[0], "%Y-%m-%d %H:%M:%S")                
                                 datetime_object_now = datetime.strptime(str(datetime.now()).split(".")[0], "%Y-%m-%d %H:%M:%S")
 
                                 time_difference = str(datetime_object_now - datetime_object_message - timedelta(hours=2)).split(":")[0]
                                 
                                 if time_difference == "2":
-                                    print("Jejeje")
                                     await channel.send("De server kan weer gebumped worden! Dit kan d.m.w het command `/bump`. Dit helpt de server groeien!")
                                     self.bumped_messages.append(message.id)
                         except Exception as error:
