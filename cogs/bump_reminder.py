d355554a58769798eaf1a0f71b25d6b98645a593
Kelvin de Reus <31496522+kelvin-codes-stuff@users.noreply.github.com>
2023-08-07 22:41:36 +0200

    Update infinite loop

--- cogs/bump_reminder.py
+++ cogs/bump_reminder.py
@@ -24,28 +24,32 @@ class Bump_reminder(commands.Cog):
     @tasks.loop(seconds=5)
     async def check_if_bump_is_ready(self):
 
-        channel = self.bot.get_channel(Channel.ALLDAYBOT)
-        messages_in_channel = await channel.history(limit=100).flatten()
+        try:
+            channel = self.bot.get_channel(Channel.ALLDAYBOT)
+            messages_in_channel = await channel.history(limit=100).flatten()
 
-        i = 0
-        for message in messages_in_channel:
-            
-            while i < 1:
-                    
+            for message in messages_in_channel:
                 if hasattr(message, "embeds"):
                     embeds = message.embeds
                     for embed in embeds:
-                        if "Check it out [on DISBOARD](https://disboard.org/server/716388937450389514)." in str(embed.description) and message.id not in self.bumped_messages:
-                            i = 1
-                            datetime_object_message = datetime.strptime(str(message.created_at).split(".")[0], "%Y-%m-%d %H:%M:%S")                
-                            datetime_object_now = datetime.strptime(str(datetime.now()).split(".")[0], "%Y-%m-%d %H:%M:%S")
+                        try:
+                            if "Check it out [on DISBOARD](https://disboard.org/server/716388937450389514)." in str(embed.description) and message.id not in self.bumped_messages:
+                                print("Jeej")
+                                datetime_object_message = datetime.strptime(str(message.created_at).split(".")[0], "%Y-%m-%d %H:%M:%S")                
+                                datetime_object_now = datetime.strptime(str(datetime.now()).split(".")[0], "%Y-%m-%d %H:%M:%S")
 
-                            time_difference = str(datetime_object_now - datetime_object_message - timedelta(hours=2)).split(":")[0]
-                            
-                            if time_difference == "2":
-                                await channel.send("De server kan weer gebumped worden! Dit kan d.m.w het command `/bump`. Dit helpt de server groeien!")
-                                self.bumped_messages.append(message.id)
-                        
+                                time_difference = str(datetime_object_now - datetime_object_message - timedelta(hours=2)).split(":")[0]
+                                
+                                if time_difference == "2":
+                                    print("Jejeje")
+                                    await channel.send("De server kan weer gebumped worden! Dit kan d.m.w het command `/bump`. Dit helpt de server groeien!")
+                                    self.bumped_messages.append(message.id)
+                        except Exception as error:
+                            print(error)
+                            pass
+        except Exception as error:
+            print(error)
+            pass
 
 
 def setup(bot: commands.Bot):
