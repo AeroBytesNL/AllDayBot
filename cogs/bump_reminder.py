9170c37ac7f205b85dd49e5bdcb2639f538b3558
Kelvin de Reus <31496522+kelvin-codes-stuff@users.noreply.github.com>
2023-08-09 15:45:46 +0200

    Working good bump

--- cogs/bump_reminder.py
+++ cogs/bump_reminder.py
@@ -9,14 +9,17 @@ class Bump_reminder(commands.Cog):
 
     
     def __init__(self, bot: commands.Bot):
+
         self.bot = bot
         print("Cog Bump reminder is loaded!")
         self.bumped_messages = []
+        self.lst_embeds_disboard = {}
 
 
 
     @commands.Cog.listener()
     async def on_ready(self):
+
         Bump_reminder.check_if_bump_is_ready.start(self)
 
 
@@ -24,32 +27,28 @@ class Bump_reminder(commands.Cog):
     @tasks.loop(seconds=5)
     async def check_if_bump_is_ready(self):
 
-        try:
-            channel = self.bot.get_channel(Channel.ALLDAYBOT)
-            messages_in_channel = await channel.history(limit=100).flatten()
+        try: 
 
-            for message in messages_in_channel:
-                if hasattr(message, "embeds"):
-                    embeds = message.embeds
-                    for embed in embeds:
-                        try:
-                            if "Check it out [on DISBOARD](https://disboard.org/server/716388937450389514)." in str(embed.description) and message.id not in self.bumped_messages:
-                                datetime_object_message = datetime.strptime(str(message.created_at).split(".")[0], "%Y-%m-%d %H:%M:%S")                
-                                print("DEBUG bump reminder 38: ", datetime_object_message)
-                                datetime_object_now = datetime.strptime(str(datetime.now()).split(".")[0], "%Y-%m-%d %H:%M:%S")
-                                print("DEBUG bump reminder 40: ",datetime_object_now - datetime_object_message - timedelta(hours=2))
-                                time_difference = str(datetime_object_now - datetime_object_message - timedelta(hours=2)).split(":")[0]
-                                
-                                if time_difference == "2" or time_difference == "3":
-                                    print("Bump ready!")
+            channel = self.bot.get_channel(Channel.ALLDAYBOT)
+
+            for message in await channel.history(limit=50).flatten():
+
+                if hasattr(message, "embeds") and message.author.id == 302050872383242240:
+                    for embed in message.embeds:
+                            if "Check it out [on DISBOARD]" in str(embed.description) and message.id not in self.bumped_messages:
+                                msg_time_object = datetime.strptime(str(message.created_at).split(".")[0], "%Y-%m-%d %H:%M:%S") + timedelta(hours=2)
+                                time_now = datetime.strptime(str(datetime.now()).split(".")[0], "%Y-%m-%d %H:%M:%S")
+                                seconds_difference = time_now - msg_time_object
+
+                                if int(seconds_difference.seconds) >= 7200 and int(seconds_difference.seconds) < 7210:
+                                    print("Server is ready to be bumped!")
                                     await channel.send("De server kan weer gebumped worden! Dit kan d.m.v het command `/bump`. Dit helpt de server groeien!")
-                                    self.bumped_messages.append(message.id)
-                        except Exception as error:
-                            print(error)
-                            pass
+                                    self.bumped_messages.append(int(message.id)) 
+
         except Exception as error:
             print(error)
             pass
+
 
 
 def setup(bot: commands.Bot):
