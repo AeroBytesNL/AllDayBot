95abd0dfb15e27b878960fb1dd0c58414dafd898
Kelvin de Reus <31496522+kelvin-codes-stuff@users.noreply.github.com>
2023-08-31 12:30:37 +0200

    [Added] Multiple release embeds

--- GitHubAction/GitHubNewReleaseMSG.py
+++ GitHubAction/GitHubNewReleaseMSG.py
@@ -3,12 +3,12 @@ from dotenv import load_dotenv
 import os
 
 webhook_url_moderator_only = os.environ["webhook_url_moderator_only"]
+webhook_url_alldaybot = os.environ["webhook_url_alldaybot"]
+
 github_release_url = "https://api.github.com/repos/kelvin-codes-stuff/AllDayBot/releases/latest"
 
 
-#
-# release info
-#
+# Release info
 github_release_repo = requests.get(github_release_url).json()
 
 changes = str(github_release_repo["body"]).split("*")
@@ -24,9 +24,7 @@ for item in changes:
 list_changes = list_changes.replace(f"- - Full Changelog- - :", "- Alle wijzigingen:")
 
 
-#
 # Webhook 
-# 
 data = {
     "username": "Github AllDayBot"
 }
@@ -37,11 +35,13 @@ data["embeds"] = [{
         "fields": [{"name": "Wijzigingen:", "value": str(list_changes)}] # For loop for release notes
 }]
 
-res = requests.post(url=webhook_url_moderator_only, json=data)
+moderator_embed = requests.post(url=webhook_url_moderator_only, json=data)
+alldaybot_embed = requests.post(url=webhook_url_alldaybot, json=data)
 
 try:
-    res.raise_for_status()
+    moderator_embed.raise_for_status()
+    alldaybot_embed.raise_for_status()
 except requests.exceptions.HTTPError as error:
     print(error)
 else:
-    print(f"Done! status code: {res.status_code}")
+    print(f"Done! status code embed 1: {moderator_embed.status_code}. Status code embed 2: {alldaybot_embed.status_code}")
