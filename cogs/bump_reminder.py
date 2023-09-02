1f455e445b93471612b26aa25d1c97e0293a029a
Kelvin de Reus <31496522+kelvin-codes-stuff@users.noreply.github.com>
2023-09-02 15:52:41 +0200

    Dubbele berichten failsave

--- cogs/bump_reminder.py
+++ cogs/bump_reminder.py
@@ -25,32 +25,36 @@ class Bump_reminder(commands.Cog):
 
     @tasks.loop(seconds=5)
     async def check_if_bump_is_ready(self):
+        try:
+            # Get the bot channel
+            channel = self.bot.get_channel(Channel.BUMP)
 
-        # Get the bot channel
-        channel = self.bot.get_channel(Channel.BUMP)
+            # Find the last bump
+            messages = list(
+            filter(
+                lambda x: x.interaction is not None and x.interaction.name == "bump", 
+                await channel.history(limit=50).flatten()
+            )
+            )
+            last_bump = max(messages, key=lambda x: x.id)
 
-        # Find the last bump
-        messages = list(
-        filter(
-            lambda x: x.interaction is not None and x.interaction.name == "bump", 
-            await channel.history(limit=50).flatten()
-        )
-        )
-        last_bump = max(messages, key=lambda x: x.id)
+            # Check if we've already processed this bump
+            if last_bump.id == self.last_processed_bump: return
 
-        # Check if we've already processed this bump
-        if last_bump.id == self.last_processed_bump: return
+            # Check if the last bump happened more than 2 hours ago
+            bump_time = datetime.fromtimestamp(((last_bump.id >> 22) + 1420070400000) / 1000.0)
+            diff = int((datetime.now() - bump_time).seconds)
+            
+            if diff >= 7192 and diff < 7200: 
+                # Send the reminder
+                await channel.send("De server kan weer gebumped worden! Dit kan d.m.v het command `/bump`. Dit helpt de server groeien!")
+                print("Server ready to be bumped")
+                self.last_processed_bump = last_bump.id
+                
+        except Exception as error:
+            print(error)
+            pass
 
-        # Check if the last bump happened more than 2 hours ago
-        bump_time = datetime.fromtimestamp(((last_bump.id >> 22) + 1420070400000) / 1000.0)
-        diff = int((datetime.now() - bump_time).seconds)
-        
-        if diff >= 7192 and diff < 7200: 
-            # Send the reminder
-            await channel.send("De server kan weer gebumped worden! Dit kan d.m.v het command `/bump`. Dit helpt de server groeien!")
-            print("Server ready to be bumped")
-            self.last_processed_bump = last_bump.id
-        
 
 
 def setup(bot: commands.Bot):
