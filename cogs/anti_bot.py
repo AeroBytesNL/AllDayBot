88430ea0316cb66286c5a5a326268b42a4a5e0d2
KelvinCodes <prive@auticodes.nl>
2024-09-19 22:06:11 +0200

    Bugfix AllDayAntiBot role kick

--- cogs/anti_bot.py
+++ cogs/anti_bot.py
@@ -1,6 +1,7 @@
 import disnake
 from disnake.ext import commands
 from env import AntiBot as AntiBotEnv
+from env import env_variable
 
 class AntiBot(commands.Cog):
     def __init__(self, bot: commands.Bot):
@@ -8,16 +9,18 @@ class AntiBot(commands.Cog):
         print("Cog anti_bot is loaded!")
 
     @commands.Cog.listener()
-    async def on_raw_member_update(self, member):
-        for role in member.roles:
+    async def on_member_update(self, before, after):
+        for role in after.roles:
             if role.id == AntiBotEnv.ANTI_BOT_ROLE_ID:
-                user = self.bot.get_user(member.id)
+                user = await self.bot.fetch_user(after.id)
+                guild = await self.bot.fetch_guild(env_variable.GUILD_ID)
+
                 await user.send("""
                                 Je bent gekicked uit All Day Tech & Gaming omdat je de automatische bot-preventie rol hebt geselecteerd. 
                                 Je kan opnieuw lid worden via onze uitnodigingslink. 
                                 Mocht je geen toegang hebben tot deze link, stuur dan een bericht naar deze bot om contact te leggen met het beheer voor een uitnodigingslink.
                                 """)
-                await user.kick(reason="Gebruiker eruit YEET ivm het selecteren van de anti bot rol (automatisch)")
+                await guild.kick(user=after, reason="Gebruiker eruit geyeet ivm het selecteren van de anti bot rol (automatisch)")
                 print("AllDayAntiBot kicked an user because he selected the anti bot role!")
 
 def setup(bot: commands.Bot):
