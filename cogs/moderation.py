1fd6b44d02d6b14e783a3f52a4a46f7d2f91820e
AutiCodes <prive@auticodes.nl>
2024-12-13 18:30:04 +0100

    Cleaned up cog moderation.py

--- cogs/moderation.py
+++ cogs/moderation.py
@@ -7,14 +7,9 @@ from database import Database
 # Make user notification system
 
 class moderation(commands.Cog):
-
-
-
     def __init__(self, bot: commands.Bot):
         self.bot = bot
         print("Cog Moderation is loaded!")
-
-
 
     # Commands
     # Mother Off All Commands
@@ -22,12 +17,9 @@ class moderation(commands.Cog):
     async def mod(self, inter):
         pass
 
-
-
     # Logging kick/ban/timeout
     @commands.Cog.listener()
     async def on_audit_log_entry_create(self, entry):
-
         # Kick 
         if entry.action == disnake.AuditLogAction.kick:
             await moderation.log_to_mod_log(self, action="Kick", reason=entry.reason, action_maker=entry.user.display_name, action_member=entry.target.display_name)
@@ -49,29 +41,21 @@ class moderation(commands.Cog):
             await moderation.log_to_mod_log(self, action="Timeout verwijderd", reason=entry.reason, action_maker=entry.user.display_name, action_member=entry.target.display_name)
             moderation.save_moderation_action_to_db(action="Timeout verwijderd", reason=entry.reason, action_maker=entry.user, action_member=entry.target)
 
-
         # Make command that gets al the bad people and returns it by number and alphabetic order
         # Make command that then can get the user from the saved DB and return all information
-
-
 
     def save_moderation_action_to_db(action, reason, action_maker, action_member):
         Database.cursor.execute(
         f"INSERT INTO moderation_log (user_id, user_name, moderation_reason, moderation_action_taker, moderation_action) VALUES ({action_member.id}, '{action_member.display_name}', '{reason}', '{action_maker.display_name}', '{action}')")
         Database.db.commit()
 
-
-
     async def log_to_mod_log(self, action, reason, action_maker, action_member):
-
         channel = self.bot.get_channel(Channel.BLACKLISTMODLOG)
 
         embed=disnake.Embed(title=f"Er is een {action} uitgevoerd!", description=f"beheerder: {action_maker}", color=disnake.Color.red())
         embed.add_field(name=f"Lid: {action_member}", value=f"**Reden: {reason}**", inline=True)
         
         await channel.send(embed=embed)
-
-
 
 def setup(bot: commands.Bot):
     bot.add_cog(moderation(bot))
