86029afda65938f2f8b848422c293238a5802fc9
AutiCodes <prive@auticodes.nl>
2024-12-15 00:33:03 +0100

    Reformatted announce cog

--- cogs/announce.py
+++ cogs/announce.py
@@ -3,24 +3,14 @@ from disnake.ext import commands
 from env import *
 from helpers.error import Log
 
-
-
 class announce(commands.Cog):
-
-
-
     def __init__(self, bot: commands.Bot):
         self.bot = bot
         Log.info("Loaded Cog announce")
 
-
-
-    # Commands aanounce admin
     @commands.default_member_permissions(moderate_members=True)
     @commands.slash_command(description="Verstuur een announcement naar een kanaal!")
     async def announce(self, inter, channel: disnake.TextChannel):
-        
-                
         class MyModal(disnake.ui.Modal):
             def __init__(custom_self_modal) -> None:
                 components = [
@@ -36,10 +26,7 @@ class announce(commands.Cog):
                 super().__init__(title="Aankondiging", custom_id="create_tag", components=components)
 
             async def callback(custom_self_modal, inter: disnake.ModalInteraction):
-                
                 content = inter.text_values["content"]
-                print(content)
-
                 guild = await self.bot.fetch_guild(env_variable.GUILD_ID)
         
                 embed=disnake.Embed(title="\n", description=str(inter.text_values["content"]), color=0xdf8cfe)
@@ -49,9 +36,7 @@ class announce(commands.Cog):
                 
                 await inter.response.send_message("Aankondiging is verstuurd!", ephemeral=True)
 
-
         await inter.response.send_modal(modal=MyModal())
-
 
 def setup(bot: commands.Bot):
     bot.add_cog(announce(bot))
