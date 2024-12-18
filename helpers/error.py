15eaa6321a2baa78d49063c3dee9118ff1bb9387
AutiCodes <prive@auticodes.nl>
2024-12-18 11:34:24 +0100

    Added enhanced logging (partly working)

--- helpers/error.py
+++ helpers/error.py
@@ -21,26 +21,40 @@ if os.path.exists(LOG_DIR) != True:
     os.mkdir(LOG_DIR)
 if os.path.exists(f"{LOG_DIR}/{LOG_NAME}") != True:
     with open(f"{LOG_DIR}/{LOG_NAME}", mode='a'): pass
-
+# If write permissions are missing
+if os.access(f"{LOG_DIR}/{LOG_NAME}", os.W_OK) != True:
+    print(f"{RED}ERROR{RESET} - Geen schrijfpermissies voor {log_path}")
 
 class Log():
     # Info logging
     def info(data):
-        logger = setup_logging(logging.DEBUG)
-        logger.info(str(data))
-        print(f"{GREEN}INFO{RESET} - {get_time_nl()} - {data}")
+        try:
+            logger = setup_logging(logging.INFO)
+            logger.info(str(data))
+            print(f"{GREEN}INFO{RESET} - {get_time_nl()} - {data}")
+        except Exception as error:
+            print(f"{RED}ERROR{RESET} - {get_time_nl()} - {error}")
+            pass
 
     # Debug logging
     def debug(data):
-        logger = setup_logging(logging.DEBUG)
-        logger.debug(str(data))
-        print(f"{BLUE}DEBUG{RESET} - {get_time_nl()} - {data}")
+        try:
+            logger = setup_logging(logging.DEBUG)
+            logger.debug(str(data))
+            print(f"{BLUE}DEBUG{RESET} - {get_time_nl()} - {data}")
+        except Exception as error:
+            print(f"{RED}ERROR{RESET} - {get_time_nl()} - {error}")
+            pass
 
     # Warning logging
     def warning(data):
-        logger = setup_logging(logging.WARNING)
-        logger.warning(str(data))
-        print(f"{YELLOW}WARNING{RESET} - {get_time_nl()} - {data}")
+        try:
+            logger = setup_logging(logging.WARNING)
+            logger.warning(str(data))
+            print(f"{YELLOW}WARNING{RESET} - {get_time_nl()} - {data}")
+        except Exception as error:
+            print(f"{RED}ERROR{RESET} - {get_time_nl()} - {error}")
+            pass
 
     # Error logging
     def error(data):
@@ -58,21 +72,44 @@ class Log():
 def get_time_nl():
     return datetime.now(pytz.timezone("Europe/Amsterdam")).strftime("%d-%m-%Y %H:%M:%S")
 
-# Setting up logging
-def setup_logging(log_level):
-    logger = logging.getLogger()
+def initialize_logger():
+    if not os.path.exists(LOG_DIR):
+        os.makedirs(LOG_DIR)
+    log_file_path = os.path.join(LOG_DIR, LOG_NAME)
+
     logging.basicConfig(
-        filename=f"{LOG_DIR}/{LOG_NAME}",
-        encoding='utf-8',
-        level=log_level,
-        format=f"{get_time_nl()} - %(levelname)s - %(message)s"
+        level=logging.INFO,
+        format="%(asctime)s - %(levelname)s - %(message)s",
+        handlers=[
+            logging.FileHandler(log_file_path, encoding="utf-8"),
+            logging.StreamHandler()  # Log ook naar console
+        ],
     )
 
-    return logger
+logging.info("Dit is een testbericht.")
 
-# Testing
-# Log.info("I have started up")
-# Log.debug("response time: 10ms")
-# Log.warning("Oeps, things didnt go 100%")
-# Log.error("Oeps, pooped my pants")
-# Log.critical("FACKKKKK")
+# Setting up logging
+def setup_logging(log_level):
+    try:
+        logger = logging.getLogger("auticodes_logging")
+
+        if not logger.handlers:
+            logger.setLevel(log_level)
+
+            file_handler = logging.FileHandler(f"{LOG_DIR}/{LOG_NAME}", encoding='utf-8')
+            file_handler.setLevel(log_level)
+            formatter = logging.Formatter(f"{get_time_nl()} - %(levelname)s - %(message)s")
+            file_handler.setFormatter(formatter)
+            logger.addHandler(file_handler)
+
+            print(f"{GREEN}INFO{RESET} - Logging naar bestand ingesteld op {LOG_DIR}/{LOG_NAME}")
+        else:
+            print(f"{YELLOW}WARNING{RESET} - Logger bestond al, handlers zijn al ingesteld.")
+
+        for handler in logger.handlers:
+            print(f"{BLUE}DEBUG{RESET} - Handler gevonden: {handler}")
+
+        return logger
+    except Exception as error:
+        print(f"{RED}ERROR{RESET} - {get_time_nl()} - {error}")
+        raise
