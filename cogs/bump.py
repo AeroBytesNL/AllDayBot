d96ed8e2c6c79c272b5bbd943f7ca01b81b37793
AutiCodes <kelvin@aerobytes.nl>
2025-01-24 22:38:04 +0100

    Possible bugfix bump reminder

--- cogs/bump.py
+++ cogs/bump.py
@@ -2,7 +2,7 @@ import disnake
 from disnake.ext import commands, tasks
 from env import *
 import time
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, timezone
 from database import Database
 from helpers.error import Log
 
@@ -38,28 +38,20 @@ class Bump_reminder(commands.Cog):
     @tasks.loop(seconds=5)
     async def check_if_bump_is_ready(self):
         try:
-            # Get the bot channel
-            channel = self.bot.get_channel(Channel.BUMP)
+            messages = [
+                msg for msg in await (await self.bot.fetch_channel(Channel.BUMP)).history(limit=50).flatten()
+                if msg.interaction and msg.interaction.name == "bump"
+            ]
 
-            # Find the last bump
-            messages = list(
-            filter(
-                lambda x: x.interaction_metadata is not None and x.interaction_metadata.name == "bump",
-                await channel.history(limit=50).flatten()
-            )
-            )
             last_bump = max(messages, key=lambda x: x.id)
 
-            # Check if we've already processed this bump
             if last_bump.id == self.last_processed_bump: return
 
-            # Check if the last bump happened more than 2 hours ago
-            bump_time = datetime.fromtimestamp(((last_bump.id >> 22) + 1420070400000) / 1000.0)
-            diff = int((datetime.now() - bump_time).seconds)
+            bump_time = last_bump.created_at
+            diff = (datetime.now(timezone(timedelta(hours=1))) - bump_time).total_seconds()
 
             if diff >= 7192 and diff < 7200: 
-                # Send the reminder
-                await channel.send("De server kan weer gebumped worden! Dit kan d.m.v. het command `/bump`. Dit helpt de server groeien! (Ook ontvang je een bonus van 60 XP!)")
+                await (await self.bot.fetch_channel(Channel.BUMP)).send("De server kan weer gebumped worden! Dit kan d.m.v. het command `/bump`. Dit helpt de server groeien! (Ook ontvang je een bonus van 60 XP!)")
                 Log.info("Server is ready to be bumped")
                 self.last_processed_bump = last_bump.id
 
@@ -76,6 +68,7 @@ class Bump_reminder(commands.Cog):
             Database.db.commit()
         except Exception as error:
             Log.error(error)
+            pass
 
 def setup(bot: commands.Bot):
     bot.add_cog(Bump_reminder(bot))
